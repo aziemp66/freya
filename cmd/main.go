2925418280a1a51f8bb51fd4fff94acb8c33ef35
Azie Melza Pratama <aziemp66@gmail.com>
2023-02-17 03:30:33 +0700

    Init Env

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/aziemp66/freya-be/common/env"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	router := gin.Default()
+	cfg := env.LoadConfig()
+
+	router.Run(":" + cfg.Port)
 }
--- common/env/init.go
+++ common/env/init.go
@@ -6,9 +6,10 @@ import (
 )
 
 type config struct {
-	Port         int    `env:"PORT,unset" envDefault:"5000"`
-	JwtSecretKey string `env:"JWT_SECRET_KEY,unset"`
-	GinMode      string `env:"GIN_MODE,unset" envDefault:"debug"`
+	Port               string `env:"PORT,unset" envDefault:"5000"`
+	JwtSecretKey       string `env:"JWT_SECRET_KEY,unset"`
+	GinMode            string `env:"GIN_MODE,unset" envDefault:"debug"`
+	FirestoreProjectId string `env:"FIRESTORE_PROJECT_ID,unset"`
 }
 
 func LoadConfig() *config {
