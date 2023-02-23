0eea2bc0894f94a0c27a9b48394f790a443d6f82
Azie Melza Pratama <aziemp66@gmail.com>
2023-02-24 00:47:10 +0700

    Working With User Repository

---
+++ internal/repository/user/init.go
@@ -0,0 +1,57 @@
+package user
+
+import (
+	"context"
+
+	userDomain "github.com/aziemp66/freya-be/internal/domain/user"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserRepositoryImplementation struct {
+	db *mongo.Database
+}
+
+func NewUserRepositoryImplementation(db *mongo.Database) *UserRepositoryImplementation {
+	return &UserRepositoryImplementation{db}
+}
+
+func (r *UserRepositoryImplementation) Insert(ctx context.Context, user userDomain.User) (err error) {
+	_, err = r.db.Collection("users").InsertOne(ctx, user)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *UserRepositoryImplementation) FindByID(ctx context.Context, id string) (user userDomain.User, err error) {
+	err = r.db.Collection("users").FindOne(ctx, bson.M{"_id": id}).Decode(&user)
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *UserRepositoryImplementation) FindByEmail(ctx context.Context, email string) (user userDomain.User, err error) {
+	err = r.db.Collection("users").FindOne(ctx, bson.M{"email": email}).Decode(&user)
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *UserRepositoryImplementation) Update(ctx context.Context, user userDomain.User) (err error) {
+	_, err = r.db.Collection("users").ReplaceOne(ctx, bson.M{"_id": user.ID}, user)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/repository/user/repository.go
@@ -0,0 +1,16 @@
+package user
+
+import (
+	"context"
+
+	userDomain "github.com/aziemp66/freya-be/internal/domain/user"
+)
+
+type Repository interface {
+	Insert(ctx context.Context, user userDomain.User) (err error)
+	FindByID(ctx context.Context, id string) (user userDomain.User, err error)
+	FindByEmail(ctx context.Context, email string) (user userDomain.User, err error)
+	Update(ctx context.Context, user userDomain.User) (err error)
+	UpdateVerifiedEmail(ctx context.Context, id string) (err error)
+	UpdatePassword(ctx context.Context, id, password string) (err error)
+}
