f0701aea464b1e156cb1d554ec43cb544520503d
Azie Melza Pratama <aziemp66@gmail.com>
2023-03-16 01:12:31 +0700

    Post & Comment Domain

---
+++ internal/domain/post/type.go
@@ -0,0 +1,17 @@
+package post
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type (
+	Post struct {
+		ID       primitive.ObjectID `bson:"_id,omitempty"`
+		Title    string             `bson:"title,omitempty"`
+		Content  string             `bson:"content,omitempty"`
+		AuthorId primitive.ObjectID `bson:"author_id,omitempty"`
+	}
+	Comment struct {
+		ID      primitive.ObjectID `bson:"_id,omitempty"`
+		Content string             `bson:"content,omitempty"`
+		PostId  primitive.ObjectID `bson:"post_id,omitempty"`
+	}
+)
