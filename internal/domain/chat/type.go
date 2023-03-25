1cb8654fab64957c27cef82b96056a3625eaf0cc
Azie Melza Pratama <aziemp66@gmail.com>
2023-03-25 20:12:24 +0700

    Creating Chat Domain

--- internal/domain/chat/type.go
+++ internal/domain/chat/type.go
@@ -4,22 +4,34 @@ import (
 	"time"
 
 	userDomain "github.com/aziemp66/freya-be/internal/domain/user"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type (
+	Appointment struct {
+		ID           primitive.ObjectID `bson:"_id,omitempty"`
+		Psychologist userDomain.User    `bson:"psychologist,omitempty"`
+		User         userDomain.User    `bson:"user,omitempty"`
+		Status       string             `bson:"status,omitempty"`
+		Date         time.Time          `bson:"date,omitempty"`
+		CreatedAt    time.Time          `bson:"created_at,omitempty"`
+		UpdatedAt    time.Time          `bson:"updated_at,omitempty"`
+	}
+
 	Chatroom struct {
-		ID           string          `bson:"_id,omitempty"`
-		Psychologist userDomain.User `bson:"psychologist,omitempty"`
-		User         userDomain.User `bson:"user,omitempty"`
-		ChatHistory  []Message       `bson:"chat_history,omitempty"`
-		CreatedAt    time.Time       `bson:"created_at,omitempty"`
-		UpdatedAt    time.Time       `bson:"updated_at,omitempty"`
+		ID             primitive.ObjectID `bson:"_id,omitempty"`
+		AppointmentID  primitive.ObjectID `bson:"appointment_id,omitempty"`
+		PsychologistID primitive.ObjectID `bson:"psychologist,omitempty"`
+		UserID         primitive.ObjectID `bson:"user,omitempty"`
+		ChatHistory    []Message          `bson:"chat_history,omitempty"`
+		CreatedAt      time.Time          `bson:"created_at,omitempty"`
+		UpdatedAt      time.Time          `bson:"updated_at,omitempty"`
 	}
 
 	Message struct {
-		SenderId  string    `bson:"sender_id,omitempty"`
-		Content   string    `bson:"content,omitempty"`
-		CreatedAt time.Time `bson:"created_at,omitempty"`
-		UpdatedAt time.Time `bson:"updated_at,omitempty"`
+		SenderID  primitive.ObjectID `bson:"sender_id,omitempty"`
+		Content   string             `bson:"content,omitempty"`
+		CreatedAt time.Time          `bson:"created_at,omitempty"`
+		UpdatedAt time.Time          `bson:"updated_at,omitempty"`
 	}
 )
