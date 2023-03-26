ed7920ebf22998d4f7869bfc85d0ca902098f9c9
Azie Melza Pratama <aziemp66@gmail.com>
2023-03-26 22:35:44 +0700

    Adding Chat Usecase Interface

---
+++ internal/usecase/chat/usecase.go
@@ -0,0 +1,23 @@
+package chat
+
+import (
+	"context"
+
+	chatDomain "github.com/aziemp66/freya-be/internal/domain/chat"
+)
+
+type Usecase interface {
+	InsertAppointment(ctx context.Context, appointment chatDomain.Appointment) (err error)
+	FindAppointmentByID(ctx context.Context, id string) (appointment chatDomain.Appointment, err error)
+	FindAppointmentByUserID(ctx context.Context, id string) (appointments []chatDomain.Appointment, err error)
+	FindAppointmentByPsychologistID(ctx context.Context, id string) (appointments []chatDomain.Appointment, err error)
+	UpdateAppointmentStatus(ctx context.Context, id string, status string) (err error)
+	InsertChatroom(ctx context.Context, chatroom chatDomain.Chatroom) (err error)
+	FindChatroomByID(ctx context.Context, id string) (chatroom chatDomain.Chatroom, err error)
+	FindChatroomByAppointmentID(ctx context.Context, id string) (chatrooms chatDomain.Chatroom, err error)
+	DeleteChatroom(ctx context.Context, id string) (err error)
+	InsertMessageToChatroom(ctx context.Context, message chatDomain.Message, chatroomId string) (err error)
+	FindAllMessagesByChatroomID(ctx context.Context, id string) (messages []chatDomain.Message, err error)
+	FindMessageByID(ctx context.Context, id string) (message chatDomain.Message, err error)
+	DeleteMessage(ctx context.Context, id string) (err error)
+}
